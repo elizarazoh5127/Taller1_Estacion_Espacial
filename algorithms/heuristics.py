dbcb3e194952c578993b7d1d8e6862a8643dbe8c
Manuel Mejia <manuelmejiaarana@gmail.com>
2026-08-28 12:47:51 -0500

    heuristics básicos para A*

--- algorithms/heuristics.py
+++ algorithms/heuristics.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from algorithms import utils
 from algorithms.problems import SystemRepairProblem
+from math import sqrt
 
 
 def nullHeuristic(state, problem=None):
@@ -21,8 +22,28 @@ def manhattanHeuristic(state, problem):
     - the nearest pending T if the robot has the kit and systems remain.
     - C if all systems have been repaired.
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    pos = state[0]
+    hasKit = state[1]
+    pendingSystems = state[2]
+    
+    if not hasKit:
+        return abs(pos[0] - problem.kitPosition[0]) + abs(pos[1] - problem.kitPosition[1])
+    
+    elif len(pendingSystems) != 0:
+        closest_t = pendingSystems[0]
+        closest_t_distance = abs(pos[0] - closest_t[0]) + abs(pos[1] - closest_t[1])
+        
+        for t in pendingSystems:
+            current_t_distance = abs(pos[0] - t[0]) + abs(pos[1] - t[1])
+            if closest_t_distance > current_t_distance:
+                closest_t = t
+                closest_t_distance = current_t_distance
+        
+        return closest_t_distance
+    
+    else:
+        return abs(pos[0] - problem.controlPosition[0]) + abs(pos[1] - problem.controlPosition[1])
+        
 
 
 def euclideanHeuristic(state, problem):
@@ -35,8 +56,27 @@ def euclideanHeuristic(state, problem):
     - the nearest pending T if the robot has the kit and systems remain.
     - C if all systems have been repaired.
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    pos = state[0]
+    hasKit = state[1]
+    pendingSystems = state[2]
+    
+    if not hasKit:
+        return sqrt((pos[0] - problem.kitPosition[0])**2 + (pos[1] - problem.kitPosition[1])**2)
+    
+    elif len(pendingSystems) != 0:
+        closest_t = pendingSystems[0]
+        closest_t_distance = sqrt((pos[0] - closest_t[0])**2 + (pos[1] - closest_t[1])**2)
+        
+        for t in pendingSystems:
+            current_t_distance = sqrt((pos[0] - t[0])**2 + (pos[1] - t[1])**2)
+            if closest_t_distance > current_t_distance:
+                closest_t = t
+                closest_t_distance = current_t_distance
+        
+        return closest_t_distance
+    
+    else:
+        return sqrt((pos[0] - problem.controlPosition[0])**2 + (pos[1] - problem.controlPosition[1])**2)
 
 
 def systemRepairHeuristic(
